ab80061371002c3784c1deaf1c1763b0e3ed7982
kforest <forest.kevin@gmail.com>
2018-10-14 22:24:32 -0400

    Add hidden file support to flowers

--- src/data/flowers.py
+++ src/data/flowers.py
@@ -18,23 +18,24 @@ def split_train_val_set(train_ratio=0.8):
     # TODO Check if already exist
 
     for cat in categories:
-        cat_raw_path = RAW_IMAGES_FOLDER + cat + "/"
-        files = os.listdir(cat_raw_path)
-        n_train = math.floor(train_ratio * len(files))
-        cat_train_dir = TRAIN_IMAGES_FOLDER + cat + "/"
-        cat_val_dir = VAL_IMAGES_FOLDER + cat + "/"
+        if not cat.startswith('.'):
+            cat_raw_path = RAW_IMAGES_FOLDER + cat + "/"
+            files = os.listdir(cat_raw_path)
+            n_train = math.floor(train_ratio * len(files))
+            cat_train_dir = TRAIN_IMAGES_FOLDER + cat + "/"
+            cat_val_dir = VAL_IMAGES_FOLDER + cat + "/"
 
-        if not os.path.exists(cat_train_dir):
-            os.makedirs(cat_train_dir)
+            if not os.path.exists(cat_train_dir):
+                os.makedirs(cat_train_dir)
 
-        if not os.path.exists(cat_val_dir):
-            os.makedirs(cat_val_dir)
+            if not os.path.exists(cat_val_dir):
+                os.makedirs(cat_val_dir)
 
-        for i, file in enumerate(files, 1):
-            if i > n_train:
-                copyfile(cat_raw_path + file, cat_val_dir + file)
-            else:
-                copyfile(cat_raw_path + file, cat_train_dir + file)
+            for i, file in enumerate(files, 1):
+                if i > n_train:
+                    copyfile(cat_raw_path + file, cat_val_dir + file)
+                else:
+                    copyfile(cat_raw_path + file, cat_train_dir + file)
 
 
 if __name__ == '__main__':
